a7cddd687ec91d77b8c4846cb9a62461a469a9a6
Niklaas Wriedt <wriedt.niklaas@gmail.com>
2024-11-23 14:02:29 +0100

    rewrite pythonWrap

--- SQLite/pythonSQLiteWrap.py
+++ SQLite/pythonSQLiteWrap.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-
 import sqlite3
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('count', type = float )
+parser.add_argument('protein', type = float )
+parser.add_argument('--food', default = "Air", type = str)
+args = parser.parse_args()
 kalorienDB = sqlite3.connect("Kalorien.db")
 
 cursor = kalorienDB.cursor()
-datum = "Datum"
-kalorien = 200
-cursor.execute(f"UPDATE Kalorien SET Kalorien ={kalorien} WHERE Datum=current_date")
-cursor.execute("SELECT Kalorien FROM Kalorien WHERE Datum=current_date")
+data = {"Count": args.count, "Protein": args.protein, "Food": args.food}
+cursor.execute("INSERT INTO Kalorien(count, protein, date, food) VALUES (:Count, :Protein, current_date, :Food )", data)
+cursor.execute("SELECT count FROM Kalorien WHERE date=current_date")
+kalorienDB.commit()
 print(cursor.fetchall())
+
